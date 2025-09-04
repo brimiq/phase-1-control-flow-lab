5e1e25c9662429e1a8cfda871125387316d94754
bradley <fazebrad13@gmail.com>
2025-09-04 14:43:23 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+
 function scuberGreetingForFeet(){
-  // Write your code here!
+  let rideLength = 0
+  let sample = 0
+  if(rideLength<=400){
+    console.log(`Sample${sample}`)
+    sample = sample + 1
+    console.log("This one is on me")
+  }
+  scuberGreetingForFeet(1500)="That will be 20 bucks."
+  if(rideLength >=400 && rideLength<=2000){
+    console.log("Your total is 20 dollars")
+  }
+  scuberGreetingForFeet(2001)="I will gladly take your 30 dollars"
+  if(rideLength>=2000 && rideLength<=2500){
+    console.log("Your total is 30 dollars")
+  }
+  scuberGreetingForFeet(2500)="No rides above 2500!"
 }
 
 function ternaryCheckCity(){
